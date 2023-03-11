420f89d1030cdfc851128c1e472fb9dc9957c1ba
Tona <tona@mango-byte.com>
2023-03-11 22:54:49 +0700

    test useDebugValue

--- 02/src/index.js
+++ 02/src/index.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
+import { useOnlineStatus } from "./helper";
+
+function StatusBar() {
+    const isOnline = useOnlineStatus();
+    return <h1>{isOnline ? '✅ Online' : '❌ Disconnected'}</h1>;
+}
 
 function Hello(props) {
-    return <h1>Hello World!</h1>;
+    return <h1>Hello { props.name }</h1>;
+}
+
+function App(props) {
+    return(
+        <div>
+            <Hello name={"Tona"}/>
+            <StatusBar />
+        </div>
+    );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Hello />);
+root.render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
